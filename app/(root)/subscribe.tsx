7a464c60de09bec5c99638e3354359256d51cfa5
Abdelwahab <ab28fb@gmail.com>
2025-03-04 17:29:33 +0100

    INIT

---
+++ app/(root)/subscribe.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const SubscribeScreen = () => {
+  return (
+    <View>
+      <Text>Subscribe Screen</Text>
+    </View>
+  );
+};
+export default SubscribeScreen;
