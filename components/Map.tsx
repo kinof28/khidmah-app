703e2591460905055d9f0dfa5325620f52ef22ca
Abdelwahab <ab28fb@gmail.com>
2025-03-12 03:54:55 +0100

    Feature: Display the user are

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,22 @@
+import { useLocationStore } from "@/store";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 const MapComponent = () => {
+  const { latitude, longitude } = useLocationStore();
+  console.log("latitude: ", latitude);
+  console.log("longitude: ", longitude);
   return (
     <MapView
-      style={{ height: "100%", width: "100%" }}
+      style={{ height: "65%", width: "100%" }}
       provider={PROVIDER_DEFAULT}
+      region={{
+        latitude: Number(latitude),
+        longitude: Number(longitude),
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,
+      }}
+      showsUserLocation={true}
+      followsUserLocation={true}
+      userLocationAnnotationTitle="You are here"
     ></MapView>
   );
 };
